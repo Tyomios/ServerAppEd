a127b02aab11351fafef0c524e5fb3fe8edfea39
Tyomios <tyomios@mail.ru>
2022-11-18 11:59:15 +0700

    Знакомство с отправкой данных в формате JSON.

--- Program.cs
+++ Program.cs
@@ -8,17 +8,15 @@ var app = builder.Build();
 
 app.Run(async (context) =>
 {
-    if (context.Request.Path == "/old")
-    {
-        context.Response.Redirect("https://www.google.com/search?q=metanit.com");
-    }
-    else if (context.Request.Path == "/new")
-    {
-        await context.Response.WriteAsync("New Page");
-    }
-    else
-    {
-        await context.Response.WriteAsync("Main Page");
-    }
+    var tom = new Person("Tom", 18);
+    context.Response.WriteAsJsonAsync(tom);
+
+    // Are equals.
+
+    //var response = context.Response; 
+    //response.Headers.ContentType = "application/json; charset=utf-8";
+    //await response.WriteAsync("{'name':'Tom', 'age':37}");
 });
 app.Run();
+
+public record Person(string Name, int Age);
