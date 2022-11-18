0ae0ce0e6b178aa53e2113e17bbe17c38f749636
Tyomios <tyomios@mail.ru>
2022-11-18 13:04:36 +0700

    Добавлене middleware для запросов.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@ using System.Text.Json;
 using HelloApp.model;
 using Microsoft.Extensions.Primitives;
 
+var users = new List<Person>
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+};
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-//app.UseWelcomePage();
 
 app.Run(async (context) =>
 {
@@ -38,3 +44,97 @@ app.Run(async (context) =>
     }
 });
 app.Run();
+
+// получение всех пользователей
+async Task GetAllPeople(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(users);
+}
+
+
+// получение одного пользователя по id
+async Task GetPerson(string? id, HttpResponse response)
+{
+    Person? user = users.FirstOrDefault((u) => u.Id.Equals(id));
+
+    if (user is not null)
+    {
+        await response.WriteAsJsonAsync(user);
+        return;
+    }
+
+    response.StatusCode = 404;
+    await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+}
+
+//удаление пользователя по Id.
+async Task DeletePerson(string? id, HttpResponse response)
+{
+    Person? user = users.FirstOrDefault((u) => u.Id.Equals(id));
+    
+    if (user is not null)
+    {
+        users.Remove(user);
+        await response.WriteAsJsonAsync(user);
+        return;
+    }
+
+    response.StatusCode = 404;
+    await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+}
+
+// Создает пользователя по полученным из запроса данным.
+async Task CreatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        var user = await request.ReadFromJsonAsync<Person>();
+        if (user is not null)
+        {
+            user.Id = Guid.NewGuid().ToString();
+            users.Add(user);
+
+            await response.WriteAsJsonAsync(user);
+            return;
+        }
+
+        throw new Exception("Некорректные данные");
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
+
+// обновление данных пользователя.
+async Task UpdatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        Person? userData = await request.ReadFromJsonAsync<Person>();
+        if (userData is not null)
+        {
+            var user = users.FirstOrDefault(u => u.Id == userData.Id);
+            
+            if (user is not null)
+            {
+                user.Age = userData.Age;
+                user.Name = userData.Name;
+                await response.WriteAsJsonAsync(user);
+                return;
+            }
+
+            response.StatusCode = 404;
+            await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+            return;
+        }
+
+        throw new Exception("Некорректные данные");
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
--- model/Person.cs
+++ model/Person.cs
@@ -1,3 +1,22 @@
 ﻿namespace HelloApp.model;
 
-public record Person(string Name, int Age);
+/// <summary>
+/// Человек.
+/// </summary>
+public class Person
+{
+    /// <summary>
+    /// Возвращает или задает идентификационный номер.
+    /// </summary>
+    public string Id { get; set; } = String.Empty;
+
+    /// <summary>
+    /// Возвращает или задает имя.
+    /// </summary>
+    public string Name { get; set; } = String.Empty;
+
+    /// <summary>
+    /// Возвращает или задает возраст.
+    /// </summary>
+    public int Age { get; set; }
+}
